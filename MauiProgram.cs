cffe25a8d92c756946177d794c328facabf97f7f
Macho <2304852hochstenbach@zuyd.nl>
2024-12-30 16:39:02 +0100

    Camera functie toegevoegd voor de JoinGamePage. De SessionPage en QuestionPage aangepast, zodat de spelregels worden overgenomen naar het daadwerkelijke spel. Ook gebruik gemaakt van 3 API's voor verschillende vragen.

--- MauiProgram.cs
+++ MauiProgram.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.Extensions.Logging;
+using CommunityToolkit.Maui;
 
 namespace TruthOrDrinkAppMac
 {
@@ -9,6 +10,7 @@ namespace TruthOrDrinkAppMac
             var builder = MauiApp.CreateBuilder();
             builder
                 .UseMauiApp<App>()
+                .UseMauiCommunityToolkitCamera()
                 .ConfigureFonts(fonts =>
                 {
                     fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
