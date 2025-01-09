4b10084e0afd67afb55701cc0b8289bbbaf08ccf
Macho <2304852hochstenbach@zuyd.nl>
2025-01-09 13:29:34 +0100

    NotificatieSettings proberen aan te maken voor de gebruikers

---
+++ MVVM/Model/UserSettings.cs
@@ -0,0 +1,18 @@
+﻿using SQLite;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TruthOrDrinkAppMac.MVVM.Model
+{
+    [Table("UserSettings")]
+    public class UserSettings
+    {
+        [PrimaryKey]
+        public int UserId { get; set; }
+        public bool NotificationsEnabled { get; set; }
+    }
+}
